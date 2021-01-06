c298f0d7a3f60f709b4b9c7fc9454ebf241b9a37
gruszczynski01 <gruszczynski01@gmail.com>
2021-01-06 21:10:40 +0100

    init backend spring boot project

---
+++ backend/src/main/java/com/wat/flatfinder/FlatfinderApplication.java
@@ -0,0 +1,13 @@
+package com.wat.flatfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatfinderApplication.class, args);
+    }
+
+}
