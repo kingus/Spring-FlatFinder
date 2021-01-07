5f873ef0288a65c38dd4ea6f57e25b5c8a556a08
kingus <kingulka.a@gmail.com>
2021-01-08 00:10:42 +0100

    change project structure and add endpoint to insert offers to db

--- backend/src/main/java/com/wat/flatfinder/FlatfinderApplication.java
+++ backend/src/main/java/com/wat/flatfinder/FlatfinderApplication.java
@@ -2,8 +2,10 @@ package com.wat.flatfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.wat.flatfinder.entities")
 public class FlatfinderApplication {
 
     public static void main(String[] args) {
