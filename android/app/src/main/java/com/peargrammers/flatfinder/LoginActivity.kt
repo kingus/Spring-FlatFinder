0a581fe4380f7060d7d199e9cda7c5ff824d7175
kingus <kingulka.a@gmail.com>
2021-03-26 21:56:37 +0100

    [FF-4][android] Improve organization among dimens and style files. Adjust login layout
    [FF-4]

--- android/app/src/main/java/com/peargrammers/flatfinder/LoginActivity.kt
+++ android/app/src/main/java/com/peargrammers/flatfinder/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.peargrammers.flatfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.login_activity.*
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        loginButton.setOnClickListener {
+
+        }
     }
 }
