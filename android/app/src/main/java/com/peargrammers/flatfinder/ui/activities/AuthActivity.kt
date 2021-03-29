e30cab6bfcb2bb9f3816136eac05cd9d0e77f34e
kingus <kingulka.a@gmail.com>
2021-03-30 00:42:00 +0200

    [FF-3][android] Add navigation between AuthActivity and HomeActivity
    [FF-3]

--- android/app/src/main/java/com/peargrammers/flatfinder/ui/activities/AuthActivity.kt
+++ android/app/src/main/java/com/peargrammers/flatfinder/ui/activities/AuthActivity.kt
@@ -1,12 +1,29 @@
 package com.peargrammers.flatfinder.ui.activities
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.peargrammers.flatfinder.R
+import com.peargrammers.flatfinder.ui.fragments.LoginFragment
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.auth_activity)
+
+        val loginFragment = LoginFragment()
+
+        replaceFragment(R.id.fragmentFL, loginFragment)
+
+    }
+
+    fun replaceFragment(id: Int, fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(id, fragment)
+            commit()
+        }
     }
 }
--- android/app/src/main/java/com/peargrammers/flatfinder/ui/fragments/LoginFragment.kt
+++ android/app/src/main/java/com/peargrammers/flatfinder/ui/fragments/LoginFragment.kt
@@ -1,18 +1,29 @@
 package com.peargrammers.flatfinder.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.peargrammers.flatfinder.R
-import kotlinx.android.synthetic.main.login_activity.*
+import com.peargrammers.flatfinder.ui.activities.AuthActivity
+import com.peargrammers.flatfinder.ui.activities.HomeActivity
+import kotlinx.android.synthetic.main.login_activity.loginButton
+import kotlinx.android.synthetic.main.login_fragment.*
 
 class LoginFragment : Fragment(R.layout.login_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val registerFragment = RegisterFragment()
+
         loginButton.setOnClickListener {
-
+            val intent = Intent (activity, HomeActivity::class.java)
+            activity?.startActivity(intent)
         }
-    }
 
+        registerTextView.setOnClickListener {
+            (activity as AuthActivity).replaceFragment(R.id.fragmentFL, registerFragment)
+        }
+
+    }
 }
